e72ea2481fa2fafc72747b5f3f6ca6954c4970f7
Taras <45391115+prystupataras@users.noreply.github.com>
2024-12-26 11:02:05 +0200

    Initial Commit

---
+++ CalendarSwiftUI/CalendarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarSwiftUIApp.swift
+//  CalendarSwiftUI
+//
+//  Created by Taras Prystupa on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
